eb5dace4131c386b91a5bf8b137c5804508edfab
garozarena <guzman.arozarena@gmail.com>
2018-03-22 14:28:38 +0100

    Add files via upload

---
+++ server/models/user.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  city: {
+    type: String,
+    required: true
+  }
+});
+var Model = mongoose.model('User', userSchema);
+module.exports = Model;
